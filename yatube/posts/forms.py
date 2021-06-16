ae84d56eae8bb447dc9e8dba08305d225c810fd5
Надежда Гурова <yudinanade@yandex.ru>
2021-06-16 15:24:50 +0300

    Проект спринта 4 - удалила init в формах

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,5 +1,4 @@
 from django.forms import ModelForm
-from django import forms
 from .models import Post
 
 
@@ -7,8 +6,3 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
-
-    def __init__(self, *args, **kwargs):
-        super(PostForm, self).__init__(*args, **kwargs)
-
-        self.fields['text'].required = True
